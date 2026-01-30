6a9ea9c4933658de4dc8b0d8801b6cfef5d084c1
Azimeh <azimehobadiah1@gmail.com>
2026-01-30 19:06:54 +0100

    update: minor UI changes to Salesinfo component

--- client/src/components/dashboard/SalesInfo.tsx
+++ client/src/components/dashboard/SalesInfo.tsx
@@ -7,7 +7,7 @@ function SalesInfo(){
         <div className="bg-white w-[57%] max-[767px]:w-full md:w-[60%] h-full max-[767px]:h-[300px] md:h-[280px] shadow border border-gray-200 rounded-2xl overflow-hidden">
             <SalesOverviewChart></SalesOverviewChart>
         </div>
-        <div className="bg-white w-[40%] max-sm:mt-5 max-[767px]:w-full md:w-[38%] max-[767px]:h-[200px] md:h-[280px] max-[767px]:mt-2 h-full shadow border border-gray-200 rounded-2xl px-5 flex-col  overflow-scroll hide-scrollbar">
+        <div className="bg-white w-[40%] max-sm:mt-5 max-[767px]:w-full md:w-[38%] max-[767px]:h-[250px] md:h-[280px] max-[767px]:mt-2 h-full shadow shadow-green-200 border border-green-300 rounded-2xl px-5 flex-col  overflow-scroll hide-scrollbar">
         <div className="sticky top-0 h-10 flex justify-between items-center">
             <h1 className="text-xs"><i className="bx bx-bar-chart-alt-2"></i> Activity Overview</h1> 
             <select name="" id="" className="text-xs">
@@ -18,7 +18,7 @@ function SalesInfo(){
         </div>
 
         <div className="w-full h-[75%] justify-between items-start flex flex-col">
-            <div className="flex flex-col items-center justify-evenly w-full h-[50%] border-b border-gray-300">
+            <div className="flex flex-col items-center justify-evenly w-full h-[50%] border-b border-green-300">
                 <h1 className="text-2xl font-bold text-gray-900">2430+</h1>
                 <p className="text-sm text-gray-800">Drugs Sold</p>
             </div>
