75240bf3593b310f43bb87d36bc09a9c9a965fb7
Azimeh <azimehobadiah1@gmail.com>
2026-01-30 19:03:08 +0100

    fix: minor updates to salesinfo component

--- client/src/components/dashboard/SalesInfo.tsx
+++ client/src/components/dashboard/SalesInfo.tsx
@@ -7,7 +7,7 @@ function SalesInfo(){
         <div className="bg-white w-[57%] max-[767px]:w-full md:w-[60%] h-full max-[767px]:h-[300px] md:h-[280px] shadow border border-gray-200 rounded-2xl overflow-hidden">
             <SalesOverviewChart></SalesOverviewChart>
         </div>
-        <div className="bg-white w-[40%] max-[767px]:w-full md:w-[38%] max-[767px]:h-[200px] md:h-[280px] max-[767px]:mt-2 h-full shadow border border-gray-200 rounded-2xl px-5 flex-col  overflow-scroll hide-scrollbar">
+        <div className="bg-white w-[40%] max-sm:mt-5 max-[767px]:w-full md:w-[38%] max-[767px]:h-[200px] md:h-[280px] max-[767px]:mt-2 h-full shadow border border-gray-200 rounded-2xl px-5 flex-col  overflow-scroll hide-scrollbar">
         <div className="sticky top-0 h-10 flex justify-between items-center">
             <h1 className="text-xs"><i className="bx bx-bar-chart-alt-2"></i> Activity Overview</h1> 
             <select name="" id="" className="text-xs">
