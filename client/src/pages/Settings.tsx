f8e5dd4f169910fbf8937dbbb6026d05fb66fb44
Azimeh <azimehobadiah1@gmail.com>
2026-02-01 12:40:47 +0100

    update: some updated tailwind css classes

--- client/src/pages/Settings.tsx
+++ client/src/pages/Settings.tsx
@@ -199,7 +199,7 @@ const Settings = ()=>{
                                             onChange={(e) => setFormData({...formData, notifications: e.target.checked})}
                                             className="sr-only peer"
                                         />
-                                        <div className={`w-11 h-6 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600 transition-colors duration-300 ${
+                                        <div className={`w-11 h-6 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600 transition-colors duration-300 ${
                                             isDarkMode
                                                 ? 'bg-gray-700 after:bg-gray-800 after:border-gray-600'
                                                 : 'bg-gray-200 after:bg-white after:border-gray-300'
@@ -219,7 +219,7 @@ const Settings = ()=>{
                                             onChange={(e) => setFormData({...formData, emailAlerts: e.target.checked})}
                                             className="sr-only peer"
                                         />
-                                        <div className={`w-11 h-6 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600 transition-colors duration-300 ${
+                                        <div className={`w-11 h-6 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600 transition-colors duration-300 ${
                                             isDarkMode
                                                 ? 'bg-gray-700 after:bg-gray-800 after:border-gray-600'
                                                 : 'bg-gray-200 after:bg-white after:border-gray-300'
@@ -239,7 +239,7 @@ const Settings = ()=>{
                                             onChange={(e) => setFormData({...formData, lowStockAlerts: e.target.checked})}
                                             className="sr-only peer"
                                         />
-                                        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
+                                        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
                                     </label>
                                 </div>
                                 
@@ -255,7 +255,7 @@ const Settings = ()=>{
                                             onChange={(e) => setFormData({...formData, expiryAlerts: e.target.checked})}
                                             className="sr-only peer"
                                         />
-                                        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
+                                        <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-green-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-0.5 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
                                     </label>
                                 </div>
                             </div>
